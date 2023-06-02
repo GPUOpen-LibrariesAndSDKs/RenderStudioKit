7e072c02193f10bd559ebb7ed67b34c7718ae917
Vsevolod Golovko <VsGolovko@luxoft.com>
2023-06-02 14:55:22 +0200

    minor change

--- CMake/modules/FindMayaUsd.cmake
+++ CMake/modules/FindMayaUsd.cmake
@@ -5,7 +5,7 @@ if (pxr_FOUND)
    find_package(PythonLibs 3 REQUIRED)
    list(APPEND CMAKE_PREFIX_PATH "${CMAKE_SOURCE_DIR}/../Boost_1_76_0/bin")
 
-   include_directories(${CMAKE_PROJECT_NAME} PRIVATE
+   include_directories(
        ${PYTHON_INCLUDE_DIRS})
 
    target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE
