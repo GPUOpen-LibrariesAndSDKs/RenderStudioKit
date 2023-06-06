53debdf72200a723e21bf57cd741080bd297e1ac
s1lentssh <s1lent.ssh@gmail.com>
2023-06-06 15:13:32 +0200

    WIP

--- Sources/Serialization/Api.cpp
+++ Sources/Serialization/Api.cpp
@@ -69,4 +69,149 @@ DeserializeDeltas(const std::string message)
     return { layerName, deltas, sequence };
 }
 
+// --- SpecData ---
+void
+tag_invoke(const value_from_tag&, value& json, const SpecData& v)
+{
+    object result;
+    result["specType"] = boost::json::value_from(v.specType);
+    auto& jsonFields = result["fields"].emplace_array();
+
+    for (const auto& field : v.fields)
+    {
+        boost::json::object jsonField;
+        jsonField["key"] = boost::json::value_from(field.first);
+        jsonField["value"] = boost::json::value_from(field.second);
+        jsonFields.push_back(jsonField);
+    }
+
+    json = result;
+}
+
+SpecData
+tag_invoke(const value_to_tag<SpecData>&, const value& json)
+{
+    object jsonObject = json.as_object();
+    array jsonFields = jsonObject.at("fields").as_array();
+
+    SpecData result;
+    result.specType = boost::json::value_to<SdfSpecType>(jsonObject.at("specType"));
+
+    for (const auto& jsonField : jsonFields)
+    {
+        TfToken key = boost::json::value_to<TfToken>(jsonField.at("key"));
+        VtValue value = boost::json::value_to<VtValue>(jsonField.at("value"));
+        result.fields.push_back({ key, value });
+    }
+
+    return result;
+}
+
+// --- DeltaEvent ---
+void
+tag_invoke(const value_from_tag&, value& json, const DeltaEvent& v)
+{
+    object result;
+    result["layer"] = boost::json::value_from(v.layer);
+    result["user"] = boost::json::value_from(v.user);
+    result["updates"] = boost::json::value_from(v.updates);
+    json = result;
+}
+
+DeltaEvent
+tag_invoke(const value_to_tag<DeltaEvent>&, const value& json)
+{
+    return DeltaEvent {
+        boost::json::value_to<std::string>(json.at("layer")), boost::json::value_to<std::string>(json.at("user")),
+        /*boost::json::value_to<TfHashMap<SdfPath, SpecData, SdfPath::Hash>>(json.at("updates")),*/
+    };
+}
+
+// --- AcknowledgeEvent ---
+void
+tag_invoke(const value_from_tag&, value& json, const AcknowledgeEvent& v)
+{
+    object result;
+    result["layer"] = boost::json::value_from(v.layer);
+    result["paths"] = boost::json::value_from(v.paths);
+    json = result;
+}
+
+AcknowledgeEvent
+tag_invoke(const value_to_tag<AcknowledgeEvent>&, const value& json)
+{
+    return AcknowledgeEvent { boost::json::value_to<std::string>(json.at("layer")),
+                              boost::json::value_to<std::vector<SdfPath>>(json.at("paths")) };
+}
+
+// --- HistoryEvent ---
+void
+tag_invoke(const value_from_tag&, value& json, const HistoryEvent& v)
+{
+    (void)v;
+    json = object {};
+}
+
+HistoryEvent
+tag_invoke(const value_to_tag<HistoryEvent>&, const value& json)
+{
+    (void)json;
+    return {};
+}
+
+// --- Event ---
+void
+tag_invoke(const value_from_tag&, value& json, const Event& v)
+{
+    object result;
+    result["event"] = boost::json::value_from(v.event);
+
+    if (v.event == "Delta::Event")
+    {
+        result["body"] = boost::json::value_from(std::get<DeltaEvent>(v.body));
+    }
+    else if (v.event == "Acknowledge::Event")
+    {
+        result["body"] = boost::json::value_from(std::get<AcknowledgeEvent>(v.body));
+    }
+    else if (v.event == "History::Event")
+    {
+        result["body"] = boost::json::value_from(std::get<HistoryEvent>(v.body));
+    }
+    else
+    {
+        throw std::runtime_error("JSON event is unsupported");
+    }
+
+    json = result;
+}
+
+Event
+tag_invoke(const value_to_tag<Event>&, const value& json)
+{
+    Event result;
+    object jsonObject = json.as_object();
+    std::string jsonEvent = boost::json::value_to<std::string>(jsonObject.at("event"));
+    result.event = jsonEvent;
+
+    if (jsonEvent == "Delta::Event")
+    {
+        result.body = boost::json::value_to<DeltaEvent>(jsonObject.at("body"));
+    }
+    else if (jsonEvent == "Acknowledge::Event")
+    {
+        result.body = boost::json::value_to<AcknowledgeEvent>(jsonObject.at("body"));
+    }
+    else if (jsonEvent == "History::Event")
+    {
+        result.body = boost::json::value_to<HistoryEvent>(jsonObject.at("body"));
+    }
+    else
+    {
+        throw std::runtime_error("JSON event is unsupported");
+    }
+
+    return result;
+}
+
 } // namespace RenderStudioApi
--- Sources/Serialization/Api.h
+++ Sources/Serialization/Api.h
@@ -21,12 +21,16 @@ namespace RenderStudioApi
 {
 
 PXR_NAMESPACE_USING_DIRECTIVE
+using namespace boost::json;
 
 struct SpecData
 {
     SdfSpecType specType = SdfSpecTypeUnknown;
     std::vector<std::pair<TfToken, VtValue>> fields;
 };
+
+void tag_invoke(const value_from_tag&, value& json, const SpecData& v);
+SpecData tag_invoke(const value_to_tag<SpecData>&, const value& json);
 
 struct DeltaEvent
 {
@@ -35,20 +39,33 @@ struct DeltaEvent
     TfHashMap<SdfPath, SpecData, SdfPath::Hash> updates;
 };
 
+void tag_invoke(const value_from_tag&, value& json, const DeltaEvent& v);
+DeltaEvent tag_invoke(const value_to_tag<DeltaEvent>&, const value& json);
+
 struct AcknowledgeEvent
 {
+    std::string layer;
     std::vector<SdfPath> paths;
 };
 
-struct HistoryLoadedEvent
+void tag_invoke(const value_from_tag&, value& json, const AcknowledgeEvent& v);
+AcknowledgeEvent tag_invoke(const value_to_tag<AcknowledgeEvent>&, const value& json);
+
+struct HistoryEvent
 {
 };
+
+void tag_invoke(const value_from_tag&, value& json, const HistoryEvent& v);
+HistoryEvent tag_invoke(const value_to_tag<HistoryEvent>&, const value& json);
 
 struct Event
 {
     std::string event;
-    std::variant<DeltaEvent, AcknowledgeEvent, HistoryLoadedEvent> body;
+    std::variant<DeltaEvent, AcknowledgeEvent, HistoryEvent> body;
 };
+
+void tag_invoke(const value_from_tag&, value& json, const Event& v);
+Event tag_invoke(const value_to_tag<Event>&, const value& json);
 
 typedef std::pair<TfToken, VtValue> _FieldValuePair;
 struct _SpecData
