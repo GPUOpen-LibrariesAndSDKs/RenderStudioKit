ddc8b49027b366a072b7708c4a3457a7fde9a6c1
Vsevolod Golovko <VsGolovko@luxoft.com>
2023-06-23 16:55:32 +0200

    Nullptr check in order to avoid a crash

--- Sources/FileFormat.cpp
+++ Sources/FileFormat.cpp
@@ -304,6 +304,10 @@ RenderStudioFileFormat::RenderStudioFileFormat()
 {
 }
 
-RenderStudioFileFormat::~RenderStudioFileFormat() { mWebsocketClient->Disconnect(); }
+RenderStudioFileFormat::~RenderStudioFileFormat() { 
+    if (mWebsocketClient) {
+        mWebsocketClient->Disconnect();
+    }
+}
 
 PXR_NAMESPACE_CLOSE_SCOPE
