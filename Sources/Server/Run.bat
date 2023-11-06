f9da38be1038bc2287ca37de7a57cd5a73a7aa00
Aleksandr Gurov <s1lent.ssh@gmail.com>
2023-11-06 21:08:43 +0100

    Fixed batch script for server (#31)

--- Sources/Server/Run.bat
+++ Sources/Server/Run.bat
@@ -7,7 +7,7 @@ set SCRIPT_DIR="%~dp0"
 :: Prepare environment
 set USD_DIR=%~dp0..
 set SERVICES_DIR=%USD_DIR%\..
-set PYTHON_DIR=%SERVICES_DIR%\Engine\StreamingApp
+set PYTHON_DIR=%SERVICES_DIR%\Engine\Standalone
 
 :: Setup environment
 set USD_PATH=%USD_DIR%\lib;%USD_DIR%\bin;%USD_DIR%\plugin\usd
